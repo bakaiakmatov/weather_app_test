55bfd99a63a61c12619c96b82f98d8cdd6515ef6
Bakai Akmatov <bakaiakmatov96@gmail.com>
2021-11-17 13:51:15 +0300

    Update weather_bloc.dart

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,32 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:weather_app_test/bloc/weather_repository.dart';
-import 'package:weather_app_test/helper/api_request.dart';
 import 'package:weather_app_test/helper/exception.dart';
 import 'package:weather_app_test/model/weather_model.dart';
-
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-
   final WeatherRepository repository;
- 
   WeatherBloc(this.repository) : super(WeatherInitial());
 
   @override
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    try{
-      if(event is GetWeatherEvent) {
+    try {
+      if (event is GetWeatherEvent) {
         yield WeatherIsLoading();
         WeatherModel data = await repository.getWeather(event.city);
         yield WeatherLoaded(data);
       }
-    }catch(e){
+    } catch (e) {
       yield WeatherError(WeatherException.catchError(e));
     }
-
   }
 }
